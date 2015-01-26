c6da677a6ce9692d3eaf168cfef2bcaa3737b13d
Pavelowed <pavelowed@gmail.com>
2015-01-26 03:39:43 +0000

    Initial commit

---
+++ routes/api.js
@@ -0,0 +1,18 @@
+module.exports = function(express, app) {
+
+var apiRouter = express.Router();
+  
+apiRouter.get('/', function(req, res, next) {
+  res.json({message:'hello world, this is api'});
+});
+  
+//Authen
+  
+  
+//Data Service
+  
+  
+  
+  
+return apiRouter;
+};
